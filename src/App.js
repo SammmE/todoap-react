57efa00c0a1835f272f49e63d297d576a9e2f5a7
SammmE <samhithenganti@gmail.com>
2022-10-02 20:46:24 -0400

    added jsdelivr imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import Router from "./components/Router";
 function App() {
     return (
         <div className="App">
+            <script src="https://cdn.jsdelivr.net/npm/@emotion/react@11.10.4"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@emotion/styled@11.10.4"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@mui/icons-material@5.10.3"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@mui/material@5.10.5"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@testing-library/jest-dom@5.16.5"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@testing-library/react@13.4.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/testing-library/user-event@13.5.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/firebase@9.10.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react@18.2.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-dom@18.2.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-firebase-hooks@5.0.3"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-router-dom@6.4.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/react-scripts@5.0.1"></script>
+            <script src="https://cdn.jsdelivr.net/npm/web-vitals@2.1.4"></script>
             <Router />
         </div>
     );
